fdd534da916a506187a29cdd7bb0d506a9d149a3
mustafa tpr <43439571+murrie030@users.noreply.github.com>
2019-12-15 22:26:47 +0100

    Removed unused imports

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,19 +1,10 @@
 package app;
-import java.util.Scanner;
-
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import parser.*;
 
 public class Main extends Application {
     @Override
